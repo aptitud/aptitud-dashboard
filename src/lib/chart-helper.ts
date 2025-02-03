145c14e4205b84ffd9138c00901bffa38c8f65b6
Jonas Ehrenfried <jonas.ehrenfried@gmail.com>
2025-02-03 16:08:15 +0100

    Improve displaying of max chart value

--- src/lib/chart-helper.ts
+++ src/lib/chart-helper.ts
@@ -8,8 +8,8 @@ export const calculateYAxisValues = (chartData: FinanceRowData[]) => {
     ...chartData.flatMap((p) => p.employees),
   ].filter((p) => p !== undefined);
 
-  const minValue = allValues.sort((a, b) => a - b)[0];
-  const maxValue = allValues.sort((a, b) => b - a)[0];
+  const minValue = Math.min(...allValues);
+  const maxValue = Math.max(...allValues);
 
   const startValue = Math.floor(minValue / 1000000) * 1000000;
   const endValue = Math.ceil(maxValue / 1000000) * 1000000;
@@ -17,5 +17,6 @@ export const calculateYAxisValues = (chartData: FinanceRowData[]) => {
     { length: (endValue - startValue) / 2000000 + 1 },
     (_, index) => startValue + index * 2000000,
   );
-  return { min: minValue, max: maxValue, ticks: millionValues };
+  // Set the max axis value a bit higher than the real max value this so that chart looks a bit nicer for the max value
+  return { min: minValue, max: maxValue + 100000, ticks: millionValues };
 };
