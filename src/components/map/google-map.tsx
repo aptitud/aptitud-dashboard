d1a3bb67897ce29f7ca1b839c4cede8ced9bd188
Peter Qwärnström <peter.qwarnstrom@aptitud.se>
2025-03-13 22:10:30 +0100

    customize map markers

--- src/components/map/google-map.tsx
+++ src/components/map/google-map.tsx
@@ -40,6 +40,8 @@ export function GoogleMap({ officeMapLocation, mapLocations, apiKey, mapId }: Go
           position: mapLocation.location,
           map,
           title: mapLocation.pointOfInterest.name,
+          content: getMarkerContent(mapLocation.pointOfInterest.type),
+          zIndex: mapLocation.pointOfInterest.type === "Aptitud" ? 100 : 0,
         });
 
         const headerContent = getHeaderContent(mapLocation.pointOfInterest.type);
@@ -67,6 +69,57 @@ export function GoogleMap({ officeMapLocation, mapLocations, apiKey, mapId }: Go
 
   return <div ref={mapRef} className="absolute w-full h-full rounded-lg shadow-md text-black" />;
 }
+
+const getMarkerContent = (type: PointOfIntrestType) => {
+  const element = document.createElement("div");
+
+  switch (type) {
+    case "Employee":
+      element.innerHTML = `
+        <div style="cursor: pointer; filter: drop-shadow(0 2px 2px rgba(0,0,0,0.3));">
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="38" viewBox="0 0 32 44">
+            <path d="M16 0C7.16 0 0 7.16 0 16c0 14.24 16 28 16 28s16-13.76 16-28C32 7.16 24.84 0 16 0z" fill="#FF3333"/>
+            <path d="M16 4C9.37 4 4 9.37 4 16c0 10.68 12 21 12 21s12-10.32 12-21c0-6.63-5.37-12-12-12z" fill="#FF6666"/>
+            <g transform="translate(8,8)">
+              <path d="M15 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2M12 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0z" 
+                stroke="white" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+            </g>
+          </svg>
+        </div>`;
+      break;
+
+    case "Customer":
+      element.innerHTML = `
+          <div style="cursor: pointer; filter: drop-shadow(0 2px 2px rgba(0,0,0,0.3));">
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="38" viewBox="0 0 32 44">
+              <path d="M16 0C7.16 0 0 7.16 0 16c0 14.24 16 28 16 28s16-13.76 16-28C32 7.16 24.84 0 16 0z" fill="#2196F3"/>
+              <path d="M16 4C9.37 4 4 9.37 4 16c0 10.68 12 21 12 21s12-10.32 12-21c0-6.63-5.37-12-12-12z" fill="#64B5F6"/>
+              <g transform="translate(4,7)" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M3 21h18"/>
+                <path d="M5 21V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16"/>
+                <path d="M9 21v-4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v4"/>
+                <path d="M8 9h8"/>
+                <path d="M8 13h8"/>
+                <path d="M8 5h8"/>
+              </g>
+            </svg>
+          </div>`;
+      break;
+
+    case "Aptitud":
+      element.innerHTML = `
+        <div style="cursor: pointer; filter: drop-shadow(0 2px 2px rgba(0,0,0,0.3));">
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="38" viewBox="0 0 32 44">
+            <path d="M16 0C7.16 0 0 7.16 0 16c0 14.24 16 28 16 28s16-13.76 16-28C32 7.16 24.84 0 16 0z" fill="#999999"/>
+            <path d="M16 4C9.37 4 4 9.37 4 16c0 10.68 12 21 12 21s12-10.32 12-21c0-6.63-5.37-12-12-12z" fill="#dddddd"/>
+            <image href="/aptitud.png" x="1" y="4" width="28" height="28"/>
+          </svg>
+        </div>`;
+      break;
+  }
+
+  return element;
+};
 
 const getHeaderContent = (type: PointOfIntrestType) => {
   switch (type) {
